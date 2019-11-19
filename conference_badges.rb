89d244ef3757cb16d79828d24059b322dd76da4a
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-19 00:43:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   badge_maker = "Hello, my name is #{name}."
 end
 def batch_badge_creator(attendees)
-  attendees = []
+  attendees_array = []
   attendees.map do |badge| "Hello, my name is #{badge}." 
 end
 end
