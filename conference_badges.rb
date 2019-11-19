2ddacab20b9684c5338e5f6c134380839342d29a
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-19 01:32:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def badge_maker(name)
+  badge_maker = "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  badge_messages = []
+  array.each do |name|
+    badge_messages << badge_maker(name)
+  end
+  badge_messages
+end
+def assign_rooms(room)
+    room = "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+    room.each {|rm| "Hello, #{name}! You'll be assigned to room #{room}!"
+  
+  end
