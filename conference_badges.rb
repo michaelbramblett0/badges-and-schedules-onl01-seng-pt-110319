cde46f41c8ef8803b750a022d89ebf8caa6fb518
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-19 01:27:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,10 +10,10 @@ def batch_badge_creator(array)
   badge_messages
 end
 def assign_rooms(room)
-    each_with_index = [1,2,3,4,5,6,7]
+    each_with_index = []
     room = "Hello, #{name}! You'll be assigned to room #{room}!"
     room.each do |rm|
-    
+    each_with_index << room
     end
     room
   end
