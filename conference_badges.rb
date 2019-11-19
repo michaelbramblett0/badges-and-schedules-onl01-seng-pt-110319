34a4100cca6c49ba248d4aaaaee4503a4052e5c8
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-19 01:06:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,9 +17,9 @@ def batch_badge_creator(array)
   end
   badge_messages
 end
-def assign_rooms(rooms)
-    room_number = "Hello, #{name}! You'll be assigned to room #{rooms}!"
-    rooms.each do |rm| 
+def assign_rooms(room)
+    room_number = "Hello, #{name}! You'll be assigned to room #{room}!"
+    room.each do |rm| 
       room_number << badge_maker(name)
     end
     room_number
