e8a393464834fc78b47bcc3ba213b8185d949b9f
Michael Bramblett <michaelbramblett0@gmail.com>
2019-11-19 00:20:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+def badge_maker(name)
+  badge_maker = "Hello, my name is #{name}."
+end
+def batch_badge_creator(array)
+  array = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+  array.each do |badge| "Hello, my name is #{badge}." 
+end
+end
